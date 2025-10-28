3e686cd0f01b82b62746dfcc7423140300e52dbc
Ben Robbins <benro2211@gmail.com>
2025-10-28 15:17:02 -0400

    get http chunks

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
 use std::{default, time::Duration};
 use anyhow::{bail, Result};
-use esp_idf_svc::{eventloop::EspSystemEventLoop, hal::prelude::Peripherals, wifi::{AuthMethod, BlockingWifi, ClientConfiguration, Configuration, EspWifi}};
-use esp_idf_hal::{delay::FreeRtos, peripheral::{self, Peripheral}, rmt::{config::TransmitConfig, FixedLengthSignal, PinState, Pulse, PulseTicks, TxRmtDriver}};
+use esp_idf_svc::{eventloop::EspSystemEventLoop, hal::prelude::Peripherals, http::{client::EspHttpConnection}, wifi::{AuthMethod, BlockingWifi, ClientConfiguration, Configuration, EspWifi}};
+use esp_idf_hal::{delay::FreeRtos, io::Read, peripheral::{self, Peripheral}, rmt::{config::TransmitConfig, FixedLengthSignal, PinState, Pulse, PulseTicks, TxRmtDriver}};
 use rgb::RGB8;
+use embedded_svc::http::client::Client;
+use embedded_svc::http::Method;
 
 // bring in secrets
 // cfg.toml generates this struct as SHOUTY_SNAKE const
 #[toml_cfg::toml_config]
 pub struct WifiConfig {
-    #[default("wifi_ssid")]
+    #[default("test")]
     wifi_ssid: &'static str,
-    #[default("wifi_password")]
+    #[default("test")]
     wifi_password: &'static str
 }
 
@@ -60,6 +62,7 @@ fn send_frame(color: u32, driver: &mut TxRmtDriver) -> Result<()> {
     Ok(())
 }
 
+// 
 fn idf_wifi(
     ssid: &str,
     password: &str,
@@ -68,12 +71,12 @@ fn idf_wifi(
 ) -> Result<Box<EspWifi<'static>>> {
     let mut auth_method = AuthMethod::WPA2Personal;
 
-    if ssid.is_empty() {
-        bail!("ssid not found")
-    }
-    if password.is_empty() {
-        bail!("password not found")
-    }
+    // if ssid.is_empty() {
+    //     bail!("ssid not found")
+    // }
+    // if password.is_empty() {
+    //     bail!("password not found")
+    // }
 
     // ASYNC wifi instance
     let mut esp_wifi = EspWifi::new(modem, sysloop.clone(), None).unwrap();
@@ -86,6 +89,11 @@ fn idf_wifi(
 
     log::info!("scanning for ap's");
     let ap_list = wifi.scan().unwrap();
+
+    // print all ap's
+    for ap in &ap_list {
+        log::info!("found ap: ssid {:?}, channel {}, auth {:?}", ap.ssid, ap.channel, ap.auth_method);
+    }
 
     // scan returns a vector of ap info structs, iterate through and match the ssid
     let ap = ap_list.into_iter().find(|found_ap| found_ap.ssid == ssid);
@@ -123,6 +131,83 @@ fn idf_wifi(
     Ok(Box::new(esp_wifi))
 }
 
+
+fn http_get(url: impl AsRef<str>) -> Result<()> {
+    // load default EspWifiConnection
+    let conn_cfg = esp_idf_svc::http::client::Configuration::default();
+    let conn = EspHttpConnection::new(&conn_cfg)?;
+    
+    let mut client = Client::wrap(conn);
+
+    let headers = [("accept", "text/plain")];
+    // create a request
+    let req = client.request(Method::Get, url.as_ref(), &headers)?;
+    // send request and store response
+    let resp = req.submit()?;
+    let status = resp.status();
+    log::info!("Response status: {}", status);
+
+    // match/map the status code to a behavior
+    match status {
+        // success status codes
+        200..=299 => {
+            // buffer for recv chunks
+            let mut buf = [0u8; 512];
+            // offset to track which 8 byte `buffer address` to write to 
+            let mut offset = 0;
+            // counter to determine len
+            let mut total = 0;
+            let mut reader = resp;
+
+            loop {
+                // read up to a 512 byte chunk, read -> Result<usize> = size
+                if let Ok(size) = Read::read(&mut reader, &mut buf[offset..]) {
+                    // response empty or data exhausted
+                    if size == 0 {
+                        break;
+                    }
+                    total += size; // add the chunk to total
+                    let size_plus_offset = size + offset;
+                    // try to convert all the bytes in the chunk up to whatever is in the chunk
+                    match str::from_utf8(&buf[..size_plus_offset]) {
+                        Ok(text) => {
+                            log::info!("Received chunk: {}", text);
+                            // reset offset
+                            offset = 0;
+                        }
+
+                        // error if from_utf8 fails, attempt to reconstruct the data
+                        Err(e) => {
+                            // create a boundary between where data is readable and not, SURPRINGSLY EASY
+                            let valid_to = e.valid_up_to();
+                            // unchecked method is unsafe but should all be printable chars
+                            unsafe {
+                                print!("{}", str::from_utf8_unchecked(&buf[..valid_to]));
+                            }
+                            // copy the remaining invalid data to the beginning of the buffer
+                            buf.copy_within(valid_to.., 0);
+                            // set the offset to after the size of the invalid data and continue reading
+                            offset = size_plus_offset - valid_to;
+                        }
+                    }
+                }
+            }
+
+            log::info!("Total bytes received: {}", total);
+
+
+        }
+        // anything else _ => bail
+        _ => 
+            bail!("HTTP request failed: {}", status),
+    }
+
+
+
+    Ok(())
+}
+
+
 fn main() -> Result<()> {   
     esp_idf_svc::sys::link_patches();
     esp_idf_svc::log::EspLogger::initialize_default();
@@ -148,9 +233,11 @@ fn main() -> Result<()> {
         }
     };
 
+    http_get("http://neverssl.com/")?;
+
     log::info!("Hello, world!");
     loop{
-        // created shifted 24 bit RGB values
+        // create shifted 24 bit RGB values
         let green = (0xFF) << 16 | 0x00 << 8 | 0x00;
         let red = 0x00 << 16 | (0xFF) << 8 | 0x00;
         let blue = 0x00 << 16 | 0x00 << 8 | (0xFF);
